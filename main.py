727612c1c9ffb322749933f004ac7a98582fc709
fluexcode <fluexcode@gmail.com>
2026-01-19 21:53:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+@app.get("/")
+async def home():
+    with open("index.html", "r", encoding="utf-8") as f:
+        return HTMLResponse(content=f.read())
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
